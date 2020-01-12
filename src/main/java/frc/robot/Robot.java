cb34726ae1d96df0a4a8c524f5547b4c3b53953d
Sagar Patil <sagarreddypatil@gmail.com>
2020-01-12 03:06:32 -0500

    undid the error thing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,8 +11,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-this code has an error now
-
 //test change
 
 /**
