7929bdab7878c8ea6e4f5c963a6c243a2b64dd5d
Sagar Patil <sagarreddypatil@gmail.com>
2020-02-06 09:56:25 -0500

    created button mappings in constants class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,12 +8,40 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
+    public static final class DriveJoystickMappings {
+        // TODO: define actual button ids later
+        public static final int intakeIn = 0; // hold to intake ball
+        public static final int runFlywheel = 1; // hold to spin flywheel
+        public static final int angleToVision = 2; // hold to keep align to vision target
+        public static final int reverseDrivetrain = 3; // toggle reverse the drive direction
+        public static final int quickTurn = 4;
+        // above is for curvature drive. see:
+        // https://first.wpi.edu/FRC/roborio/release/docs/java/edu/wpi/first/wpilibj/drive/DifferentialDrive.html#curvatureDrive(double,double,boolean)
+    }
+
+    public static final class ButtonJoystickMappings {
+        // TODO: define actual button ids later
+        public static final int intakeIn = 0; // hold to intake ball
+        public static final int intakeOut = 1; // hold to release ball in intake
+        public static final int setFlywheelClosedLoop = 2; // toggle if flywheel should be velocity closed loop or not
+        public static final int runFlywheel = 3; // hold to spin the flywheel
+        public static final int conveyorIn = 4; // hold to make conveyor go in
+        public static final int conveyorOut = 5; // hold to make conveyor go out
+        public static final int elevatorUp = 6; // hold to make elevator go up
+        public static final int elevatorDown = 7; // hold to make elevator go down
+        public static final int setElevatorTop = 8; // press to set elevator to top using limit switch
+        public static final int setElevatorButtom = 9; // press to set elevator to bottom using limit switch
+        public static final int colorWheelColorTurn = 10; // press to spin the color wheel to a certain color
+        public static final int colorWheelSpinTurn = 11; // press to spin the color wheel 4 times
+    }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,8 +8,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -22,6 +25,10 @@ public class RobotContainer {
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
+
+  Joystick driveJoystick = new Joystick(0);
+  Joystick buttonJoystick = new Joystick(1);
+
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
@@ -34,6 +41,9 @@ public class RobotContainer {
    * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
+    // example on how to use the drive mappings in constants class:
+    // new JoystickButton(buttonJoystick,
+    // Constants.ButtonJoystickMappings.intakeIn).whileHeld(new InstantCommand());
   }
 
   /**
