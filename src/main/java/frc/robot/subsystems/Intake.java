2afcf313a15e7aa4b1316763d28c80b87924fc9e
Arnav Borborah <arnavborborah11@gmail.com>
2020-02-13 22:21:11 -0500

    Create shooter constants

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -10,21 +10,17 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstants;
 
 public class Intake extends SubsystemBase {
+  private VictorSP intake = new VictorSP(IntakeConstants.intakePort);
 
-  private VictorSP intake = new VictorSP(0);
-
-  /**
-   * Creates a new Intake.
-   */
   public Intake() {
     SmartDashboard.putNumber("Intake Multiplier", 1);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
   }
 
   public void start(boolean inverse) {
