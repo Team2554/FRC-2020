9b78533f4b3a2f96aeaaf054577911f12f070150
Ved Soolgiri <ved.soolgiri@gmail.com>
2020-02-27 19:58:42 +0000

    Mrs. B

--- src/main/java/frc/robot/commands/Conveyor/WhenConveyorIn.java
+++ src/main/java/frc/robot/commands/Conveyor/WhenConveyorIn.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj.Timer;
 public class WhenConveyorIn extends CommandBase {
   private Timer baseTime = new Timer();
   private boolean timeFail = false;
-  private float timeTaken = 1000;
+  private float timeTaken = 1;
   private final Conveyor m_conveyor;
   public static double checkTime;
 
