59908c9b8def3f3904da4c3f73c2bcfa48c37fd5
Arnav Borborah <arnavborborah11@gmail.com>
2020-02-29 13:06:37 -0500

    Remove timer related code from regular conveyor commands

--- src/main/java/frc/robot/commands/TopConveyor/TopConveyorIn.java
+++ src/main/java/frc/robot/commands/TopConveyor/TopConveyorIn.java
@@ -11,14 +11,10 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.ConveyorConstants;
 import frc.robot.subsystems.TopConveyor;
 import frc.robot.subsystems.Shooter;
-import edu.wpi.first.wpilibj.Timer;
 
 public class TopConveyorIn extends CommandBase {
-  private final double timeTaken = ConveyorConstants.stopTime;
   private final TopConveyor m_conveyor;
   private final Shooter m_shooter;
-  public static double checkTime;
-  Timer ConveyorTimer = new Timer();
 
   /**
    * Creates a new TopConveyorIn.
@@ -32,14 +28,11 @@ public class TopConveyorIn extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    ConveyorTimer.start();
-
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    checkTime = Timer.getFPGATimestamp();
     m_conveyor.conveyorIn();
   }
 
@@ -53,6 +46,6 @@ public class TopConveyorIn extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (ConveyorTimer.get() >= timeTaken || !m_shooter.isShootable());
+    return !m_shooter.isShootable();
   }
 }
--- src/main/java/frc/robot/commands/TopConveyor/TopConveyorOut.java
+++ src/main/java/frc/robot/commands/TopConveyor/TopConveyorOut.java
@@ -7,16 +7,13 @@
 
 package frc.robot.commands.TopConveyor;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.TopConveyor;
 import frc.robot.subsystems.Shooter;
 
 public class TopConveyorOut extends CommandBase {
   private final TopConveyor m_conveyor;
-  private final float timeTaken = 1;
   private final Shooter m_shooter;
-  private static double checkTime;
 
   /**
    * Creates a new TopConveyorOut.
@@ -30,13 +27,11 @@ public class TopConveyorOut extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    checkTime = Timer.getFPGATimestamp();
     m_conveyor.conveyorOut();
   }
 
@@ -49,6 +44,6 @@ public class TopConveyorOut extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (checkTime > timeTaken);
+    return !m_shooter.isShootable();
   }
 }
