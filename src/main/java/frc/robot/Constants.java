752ea37fb641c847f1626c2a54c33b7ed881b862
Ved Soolgiri <ved.soolgiri@gmail.com>
2020-03-07 12:48:11 -0500

    added velocity pid

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -95,6 +95,15 @@ public final class Constants {
   public static final class ShooterConstants {
     public static final int talonPort = 4;
     public static final int shooterVoltage = 11;
+    public static final int kSlotIdx = 0;
+    public static final int kPIDLoopIdx = 0;
+    public static final int kTimeoutMs = 30;
+    public static final double kP = 0.25;
+    public static final double kI = 0.01;
+    public static final double kD = 20;
+    public static final double kF = 1023.0 / 7200.0;
+    public static final int kIzone = 300;
+    public static final double kPeakOutput = 1.00;
   }
 
   public static final class ConveyorConstants {
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -13,6 +13,7 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.ShooterConstants;
 
 public class Shooter extends SubsystemBase {
   private final TalonSRX shootMotor;
@@ -22,13 +23,25 @@ public class Shooter extends SubsystemBase {
    */
   public Shooter() {
     shootMotor = new TalonSRX(Constants.ShooterConstants.talonPort);
+    shootMotor.configFactoryDefault();
     shootMotor.setSensorPhase(true);
     shootMotor.enableVoltageCompensation(true);
     shootMotor.configVoltageCompSaturation(11);
+    shootMotor.configNominalOutputForward(0, ShooterConstants.kTimeoutMs);
+    shootMotor.configNominalOutputReverse(0, ShooterConstants.kTimeoutMs);
+    shootMotor.configPeakOutputForward(1, ShooterConstants.kTimeoutMs);
+    shootMotor.configPeakOutputReverse(-1, ShooterConstants.kTimeoutMs);
+    shootMotor.config_kF(ShooterConstants.kPIDLoopIdx, ShooterConstants.kF, ShooterConstants.kTimeoutMs);
+    shootMotor.config_kP(ShooterConstants.kPIDLoopIdx, ShooterConstants.kP, ShooterConstants.kTimeoutMs);
+    shootMotor.config_kI(ShooterConstants.kPIDLoopIdx, ShooterConstants.kI, ShooterConstants.kTimeoutMs);
+    shootMotor.config_kD(ShooterConstants.kPIDLoopIdx, ShooterConstants.kD, ShooterConstants.kTimeoutMs);
+
   }
 
   public void startMotor(final double demand) {
     shootMotor.set(ControlMode.PercentOutput, demand);
+    double target = demand * 500.0 * 4096 / 600;
+    shootMotor.set(ControlMode.Velocity, target);
   }
 
   public int getSpeed() {
@@ -44,6 +57,6 @@ public class Shooter extends SubsystemBase {
   }
 
   public void stop() {
-    shootMotor.set(ControlMode.PercentOutput, 0);
+    shootMotor.set(ControlMode.Velocity, 0);
   }
 }
